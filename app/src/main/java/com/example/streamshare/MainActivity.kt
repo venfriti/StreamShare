745586dc6a6fb2b7d559c261e4e100ed5853f832
venfriti <tolusanusi6@gmail.com>
2023-12-01 15:25:43 +0100

    feat: include streaming screen recording to a server

--- app/src/main/java/com/example/streamshare/MainActivity.kt
+++ app/src/main/java/com/example/streamshare/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.streamshare
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
@@ -10,7 +14,9 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.streamshare.service.DisplayService
@@ -31,6 +37,17 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
 
     private lateinit var displayServiceResultLauncher: ActivityResultLauncher<Intent>
 
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    private val PERMISSIONS_A_13 = arrayOf(
+        Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA,
+        Manifest.permission.POST_NOTIFICATIONS
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +61,7 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        requestPermissions()
 
         startStopButton = findViewById(R.id.start_stop_button)
         enterUrl = findViewById(R.id.enter_url)
@@ -53,16 +71,14 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
         ) { result ->
             Log.e("MainActivity", result.resultCode.toString())
             val reCode = result.resultCode
-            if (result.data != null && (
-                        reCode == REQUEST_CODE_STREAM || reCode == REQUEST_CODE_RECORD
-                    )
+            if (result.data != null && (reCode == RESULT_OK)
                 ) {
                 // Handle the result here
                 val data: Intent? = result.data
                 val displayService = DisplayService.INSTANCE
                 if (displayService != null){
                     val endpoint: String = enterUrl.text.toString()
-                    displayService.prepareStreamRtp(endpoint, result.resultCode, data!!)
+                    displayService.prepareStreamRtp(endpoint, reCode, data!!)
                     displayService.startStreamRtp(endpoint)
                 }
             } else {
@@ -107,6 +123,44 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
         if (displayService != null && !displayService.isStreaming() && !displayService.isRecording()) {
             //stop service only if no streaming or recording
             stopService(Intent(this, DisplayService::class.java))
+        }
+    }
+
+    private fun requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!hasPermissions(this)) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_A_13, 1)
+            }
+        } else {
+            if (!hasPermissions(this)) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, 1)
+            }
+        }
+    }
+
+    private fun showPermissionsErrorAndRequest() {
+        Toast.makeText(this, "You need permissions before", Toast.LENGTH_SHORT).show()
+        requestPermissions()
+    }
+
+    private fun hasPermissions(context: Context?, vararg permissions: String): Boolean {
+        if (context != null) {
+            for (permission in permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun hasPermissions(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            hasPermissions(context, *PERMISSIONS_A_13)
+        } else {
+            hasPermissions(context, *PERMISSIONS)
         }
     }
 
