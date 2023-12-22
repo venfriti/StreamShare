fe397bd78389945e99593ed46b631ba7aec49215
venfriti <tolusanusi6@gmail.com>
2023-12-22 12:11:05 +0100

    wait

--- app/src/main/java/com/example/streamshare/MainActivity.kt
+++ app/src/main/java/com/example/streamshare/MainActivity.kt
@@ -19,22 +19,36 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.DrawableRes
 import androidx.annotation.RequiresApi
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
@@ -134,38 +148,6 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
         }
     }
 
-    @Composable
-    fun StreamShareLayout(){
-        Column(
-            modifier = Modifier
-                .statusBarsPadding()
-                .padding(horizontal = 40.dp)
-                .verticalScroll(rememberScrollState())
-                .safeDrawingPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(id = R.string.enter_url),
-                modifier = Modifier
-                    .padding(bottom = 16.dp, top = 40.dp)
-                    .align(alignment = Alignment.Start)
-            )
-        }
-    }
-
-    @Preview(showSystemUi = true)
-    @Composable
-    fun StreamShareLayoutPreview() {
-        StreamShareTheme {
-            Surface(
-                Modifier.fillMaxSize()
-            ) {
-                StreamShareLayout()
-            }
-        }
-    }
-
     private fun showSnackBar() {
         Snackbar.make(
             findViewById(
@@ -226,13 +208,6 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
         }
         else {
             showPermissionSnackBar()
-        }
-    }
-    fun switchBack() {
-        val originalAppLaunchIntent = packageManager.getLaunchIntentForPackage(Constants.APP_PACKAGE_NAME)
-        originalAppLaunchIntent?.let {
-            startActivity(it)
-            finish()
         }
     }
 
@@ -325,5 +300,76 @@ class MainActivity : AppCompatActivity(), ConnectChecker {
     override fun onAuthSuccess() {
         Toast.makeText(this, getString(R.string.auth_success), Toast.LENGTH_SHORT).show()
     }
+
+    @Composable
+    fun StreamShareLayout(){
+        var urlInput by remember { (mutableStateOf("")) }
+
+        Column(
+            modifier = Modifier
+                .statusBarsPadding()
+                .padding(horizontal = 40.dp)
+                .verticalScroll(rememberScrollState())
+                .safeDrawingPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(id = R.string.enter_url),
+                modifier = Modifier
+                    .padding(bottom = 16.dp, top = 40.dp)
+                    .align(alignment = Alignment.Start)
+            )
+            EditUrlField(
+                R.string.hint_url,
+                R.drawable.notification_icon,
+                urlInput,
+                onValueChanged = {urlInput = it},
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Uri,
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+
+    @Composable
+    fun EditUrlField(
+        @StringRes label: Int,
+        @DrawableRes leadingIcon: Int,
+        value: String,
+        onValueChanged: (String) -> Unit,
+        keyboardOptions: KeyboardOptions,
+        modifier: Modifier = Modifier
+    ){
+        TextField(
+            value = value,
+            onValueChange = onValueChanged,
+            leadingIcon = { Icon(
+                painter = (painterResource(id = leadingIcon)), null,
+                modifier = Modifier.size(32.dp)
+            ) },
+            label = { Text(stringResource(id = label))},
+            singleLine = true,
+            keyboardOptions = keyboardOptions,
+            modifier = modifier
+        )
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun StreamShareLayoutPreview() {
+        StreamShareTheme {
+            Surface(
+                Modifier.fillMaxSize()
+            ) {
+                StreamShareLayout()
+            }
+        }
+    }
+
 
 }
